943c919acbcd548a2f6f37d699a7bc0d0c241390
juris-97 <juris-97@yandex.ru>
2021-01-13 01:25:27 +0200

    modyfied

--- db.sh
+++ db.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-docker exec -i prestashop_mariadb_1 mysql --user root --password=root prestashop < ./dump.sql
+#docker exec -i prestashop_mariadb_1 mysql --user root --password=root prestashop < ./dump.sql
+
+DATABASE_NAME="db_9"
+DATABASE_USER="user_9"
+DATABASE_PASSWORD="password"
+
+
+docker exec -i prestashop_mariadb_1 mysql --user root --password=root -e "CREATE DATABASE IF NOT EXISTS db_9;"
+docker exec -i prestashop_mariadb_1 mysql --user root --password=root -e "CREATE USER IF NOT EXISTS user_9@'%' IDENTIFIED BY 'password';"
+docker exec -i prestashop_mariadb_1 mysql --user root --password=root -e "GRANT ALL PRIVILEGES ON db_9.* TO 'user_9'@'%';"
+docker exec -i prestashop_mariadb_1 mysql --user root --password=root -e "FLUSH PRIVILEGES;"
+
+docker exec -i prestashop_mariadb_1 mysql --user user_9 --password=password db_9 < ./docker/mariadb/db_9.sql
--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sed -i "s|'localhost'|'mariadb'|g" ./prestashop/config/settings.inc.php
-sed -i "s|'P@ssw0rd'|'root'|g" ./prestashop/config/settings.inc.php
+_DB_SERVER_=mariadb
+_DB_NAME_=prestashop
+_DB_USER_=root
+_DB_PASSWD_=root
+_DB_PREFIX_=ps_
 
-
+sed -i "s|'localhost'|'${_DB_SERVER_}'|g" /var/www/html/config/settings.inc.php
+sed -i "s|'prestashop'|'${_DB_NAME_}'|g" /var/www/html/config/settings.inc.php
+sed -i "s|'P@ssw0rd'|'${_DB_PASSWD_}'|g" /var/www/html/config/settings.inc.php
+sed -i "s|'ps_'|'${_DB_PREFIX_}'|g" /var/www/html/config/settings.inc.php
