81d8a053d8fa3a7eda4286f8422b0fb1fd3b44d0
juris-97 <juris-97@yandex.ru>
2021-01-13 02:13:01 +0200

    m

--- docker/mariadb/db_9.sql
+++ docker/mariadb/db_9.sql
@@ -20,8 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `prestashop`
 --
-CREATE DATABASE IF NOT EXISTS `prestashop` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `prestashop`;
+--CREATE DATABASE IF NOT EXISTS `prestashop` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+--USE `prestashop`;
 
 -- --------------------------------------------------------
 
