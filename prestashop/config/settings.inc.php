c215cd90c51016230dd87679914eed8990e8be97
juris-97 <juris-97@yandex.ru>
2021-01-15 02:12:02 +0200

    settings changed

--- prestashop/config/settings.inc.php
+++ prestashop/config/settings.inc.php
@@ -1,8 +1,8 @@
 <?php
 define('_DB_SERVER_', 'biznes-db');
 define('_DB_NAME_', 'db_9');
-define('_DB_USER_', 'root');
-define('_DB_PASSWD_', 'root');
+define('_DB_USER_', 'user_9');
+define('_DB_PASSWD_', 'password');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
